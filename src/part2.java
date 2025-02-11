5d084c6e2f49bbf3ff5c4626dc2852c05f904925
salahmomani <slhalmomani@gmail.com>
2025-02-11 18:09:10 +0300

    first commit

---
+++ src/part2.java
@@ -0,0 +1,264 @@
+import java.util.Scanner;
+
+public class part2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+/// ///////////////////////////Squared//////////////////
+//        int sqr=in.nextInt();
+//        System.out.println(sqr*sqr);
+
+        /// /////////////////Square root of sum////////////////
+//        int num1=in.nextInt();
+//        int num2=in.nextInt();
+//        int sum=num1+num2;
+//        System.out.println(Math.sqrt(sum));
+//
+
+        /////// ///////////////////////Absolute Value//////////////////////////
+//    int abs=in.nextInt();
+//        System.out.println(Math.abs(abs));
+
+/// /////////////////////////////////////Comparing Numbers////////////////////
+
+//        int num1=in.nextInt();
+//        int num2=in.nextInt();
+//
+//        if(num1>num2)
+//        {
+//
+//            System.out.println(num1+" is greater than "+num2);
+//        } else if (num2>num1) {
+//            System.out.println(num1+" is smaller than "+num2);
+//
+//        }
+//        else {
+//            System.out.println(num1+" is equal to"+num2);
+//        }
+
+
+
+//        /// ////////////////////Carry on?/////////////////
+//while (true)
+//{
+//    System.out.println("Shall we carry on?");
+//    String carry= in.nextLine();
+//    if(carry.equals("No"))
+//    {break;}
+//
+//}
+
+
+        /// /////////////////////Are we there yet?////////////////
+//while (true)
+//{
+//    System.out.println("Give a number:");
+//    int val= in.nextInt();
+//    if(val==4)
+//    {break;}
+//
+//}
+
+
+///// //////////////////////////Only positives/////////////////////
+//while (true)
+//{
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if(num==0)
+//    {break;}
+//    else if (num<0) {
+//        System.out.println("Unsuitable number");
+//    }
+//    else {
+//        System.out.println(Math.pow(num,2));
+//    }
+//
+//}
+/// ////////////////////Number of Numbers////////////////
+//
+//int count=0;
+//
+//while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else {count++;}
+//
+//}
+//        System.out.println("Number of numbers: "+count);
+
+
+ ////////////////////////Number of negative numbers///////////////
+//        int count=0;
+//
+//        while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else if (num<0) {
+//        count++;
+//    }
+//
+//}
+//        System.out.println("Number of numbers: "+count);
+/// /////////////////////////////////////////Sum of Numbers///////////////////////
+
+///////////////////////Number of negative numbers///////////////
+//        int sum=0;
+//
+//        while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else{
+//        sum+=num;
+//    }
+//
+//}
+//        System.out.println("Sum of the numbers:  "+sum);
+
+/// /////////////////Number and sum of numbers////////////////////
+//    int sum=0;
+//    int count=0;
+//         while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else{
+//        sum+=num;
+//        count++;
+//    }
+//
+//}
+//        System.out.println("Number of numbers: :  "+count);
+//
+//        System.out.println("Sum of the numbers:  "+sum);
+/// //////////////////////Average of numbers/////////////////
+//
+//    int sum=0;
+//    int count=0;
+//    double avg;
+//         while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else{
+//        sum+=num;
+//        count++;
+//    }
+//
+//}
+//         avg= (double) sum /count;
+//        System.out.println("Average of the numbers:  "+avg);
+
+/// ///////////////////////////Average of positive numbers////////////////////\
+//
+//        double avg;
+//    int sum=0;
+//    int count=0;
+//         while (true){
+//    System.out.println("Give a number:");
+//    int num= in.nextInt();
+//    if (num==0)
+//    {break;}
+//    else if(num>0){
+//        sum+=num;
+//        count++;
+//    }
+//
+//}
+//         if(count==0)
+//         {
+//             System.out.println("Cannot calculate the average");
+//         }
+//         else {
+//             avg = (double) sum / count;
+//             System.out.println(avg);
+//         }
+///////////////////////////////////////////Counting//////////////////
+//        int count=in.nextInt();
+//
+//        for (int i=0;i<=count;i++)
+//        {
+//            System.out.println(i);
+//
+//        }
+
+/// ///////////////////////////////////Counting to hundred//////////////////
+//
+//        int count=in.nextInt();
+//
+//        for (int i=count;i<=100;i++)
+//        {
+//            System.out.println(i);
+//
+//        }
+
+/// //////////////From where to where? (2 parts)///////////////Part1
+//
+//        System.out.println("Where to?");
+//        int num=in.nextInt();
+//
+//        for (int i=1;i<=num;i++)
+//        {
+//            System.out.println(i);
+//
+//        }
+///// //////////////From where to where? (2 parts)///////////////Part2
+//        System.out.println("Where to?");
+//        int num1=in.nextInt();
+//
+//        System.out.println("Where from?");
+//        int num2=in.nextInt();
+//
+//        for (int i=num2;i<=num1;i++)
+//        {
+//            System.out.println(i);
+//
+//        }
+///// //////////////////////////////Sum of a sequence////////////////////////
+//        int sum=0;
+//        System.out.print("Last number?");
+//        int num=in.nextInt();
+//        for(int i=1;i<=num;i++)
+//        {
+//            sum+=i;
+//        }
+//        System.out.println("the sum is "+sum);
+
+
+        /// //////////////////////////////Sum of a sequence - the sequel//////
+//        int sum=0;
+//        System.out.println("First number? ");
+//        int num1=in.nextInt();
+//        System.out.println("last number? ");
+//        int num2=in.nextInt();
+//
+//        for (int i=num1;i<=num2;i++)
+//        {
+//            sum+=i;
+//
+//        }
+//        System.out.println("the sum is "+sum);
+
+//        /// ////////////////////////////Factorial///////////////
+//        int fact=1;
+//        System.out.print("Give a number:");
+//        int num=in.nextInt();
+//        for(int i=1;i<=num;i++)
+//        {
+//            fact*=i;
+//        }
+//        System.out.println("the Factorial is "+fact);
+
+/// ////////////Repeating, breaking and remembering (5 parts)/////////////
+        /// ///////////////////part1///////////////////////
+
+
+    }
+}
