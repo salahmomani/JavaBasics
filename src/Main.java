c0f4a9fc4aa3894565f6624817e04d69df1c8fab
salahmomani <slhalmomani@gmail.com>
2025-02-11 16:21:04 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,327 @@
+import java.util.Objects;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+//
+//        System.out.println("Hello, World!");
+//        System.out.println("Ada Lovelace");
+//        System.out.println("Once upon a time\nthere was\na program\n");
+//        System.out.println("Write a message:");
+//        String test1=in.nextLine();
+//        System.out.println(test1);
+//        System.out.println("Write a message:");
+//        String test2=in.nextLine();
+//        System.out.println(test2);
+//        String name = "Ada Lovelace";
+//        System.out.println(name);
+//        System.out.println("Write a message: ");
+//        String test3=in.nextLine();
+//        System.out.println(test3);
+//        System.out.println(test3);
+//        System.out.println("What's your name?");
+//        String test4=in.nextLine();
+//        System.out.println("Hi"+test4);
+//        System.out.println("Greetings! How are you doing?");
+//        String test5=in.nextLine();
+//        System.out.println(test5);
+//        System.out.println("Oh, how interesting. Tell me more!");
+//        String test6=in.nextLine();
+//        System.out.println(test6);
+//        System.out.println("Thanks for sharing!\n");
+//        System.out.println("I will tell you a story, but I need some information first.\n" +"What is the main character called?");
+//        String bob=in.nextLine();
+//        System.out.println("What is their job?");
+//        String builder=in.nextLine();
+//        System.out.println("Here is the story:\n" +
+//                "Once upon a time there was "+bob+", who was "+builder+".\n" +
+//                "On the way to work, "+bob+"reflected on life.\n" +
+//                "Perhaps "+bob+ "will not be "+builder+" forever.");
+//        int chic=3;
+//        double bac=5.5;
+//       String no="None!";
+//        System.out.println("Chicken\n"+chic);
+//        System.out.println("Bacon(Kg)\n"+bac);
+//        System.out.println("Tractor\n"+no);
+//        System.out.println("And finally, a summary:\n"+chic+"\n"+bac+"\n"+no);
+//
+//        System.out.println("Give a number:");
+//        int num1= in.nextInt();
+//        System.out.println("You gave the number "+num1);
+//
+//        System.out.println("Give a number:");
+//        double num2=in.nextDouble();
+////        System.out.println("You gave the number "+num2);
+//        System.out.println("Write something:");
+//        boolean value = Boolean.valueOf(in.nextLine());
+//        System.out.println("True or false? "+value);
+//
+//        System.out.println("Give a string:");
+//        String bye=in.nextLine();
+//        System.out.println("Give a integer:");
+//        int num7=in.nextInt();
+//        System.out.println("Give a double:");
+//        double dou2=in.nextDouble();
+//        System.out.println("Give a boolean:");
+//        boolean bol2=in.nextBoolean();
+//        System.out.println("You gave the string " + bye+"\n"+"You gave the integer "+num7+"\n" +
+//                "You gave the double "+dou2+"\n" +
+//                "You gave the boolean "+bol2);
+
+//        System.out.println("How many days would you like to convert to seconds?");
+//        int second =in.nextInt();
+//        System.out.println(second*24*60*60);
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        int sum=first+second;
+//        System.out.println("The sum of the numbers is "+sum);
+//
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        System.out.println("Give the third number:");
+//        int third =in.nextInt();
+//        int sum=first+second+third;
+//        System.out.println("The sum of the numbers is "+sum);
+
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        int sum=first+second;
+//        System.out.println(first+" 5+ "+second+" ="+sum);
+
+
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        int mul=first*second;
+//        System.out.println(first+" + "+second+" ="+mul);
+
+
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        int avg=(first+second)/2;
+//        System.out.println("The average is " +avg);
+
+
+
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        System.out.println("Give the third number:");
+//        int third =in.nextInt();
+//        double avg=1.0*(first+second+third)/3;
+//        System.out.println("The sum of the numbers is "+avg);
+//
+//
+
+
+
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//        System.out.println(first+" + "+second +" = "+(first+second) );
+//        System.out.println(first+" - "+second +" = "+(first-second) );
+//        System.out.println(first+" * "+second +" = "+(first*second) );
+//        System.out.println(first+" / "+second +" = "+(first/second) );
+
+
+//        System.out.println("Give speed:");
+//        int speed=in.nextInt();
+//        if(speed>120){
+//            System.out.println("Speeding ticket!");
+//        }
+
+//        System.out.println("Give number:");
+//        int year=in.nextInt();
+//        if(year==1984){
+//        System.out.println("Orwell");
+
+
+
+//        System.out.println("Give number:");
+//        int year=in.nextInt();
+//        if(year<2015){
+//            System.out.println("Ancient history!");
+
+
+//
+//        System.out.println("Give number:");
+//        int PNnum=in.nextInt();
+//        if(PNnum<0){
+//            System.out.println("The number is not positive");
+//        }
+//        else if(PNnum>0) {
+//            System.out.println("The number is positive");
+//        }
+//else{
+//            System.out.println("Number is 0");
+//        }
+
+
+//
+//        System.out.println("How old are you?");
+//        int old=in.nextInt();
+//        if(old<18){
+//            System.out.println("You are not an adult");
+//        }
+//        else {
+//            System.out.println("You are an adult" );
+//        }
+
+//
+//        System.out.println("Give the first number:");
+//        int first =in.nextInt();
+//        System.out.println("Give the second number:");
+//        int second =in.nextInt();
+//
+//        if(first>second){
+//            System.out.println("Greater number is: "+first);
+//        }
+//        else if(second>first){
+//            System.out.println("Greater number is: "+second);
+//        }
+//        else{
+//            System.out.println("The numbers are equal!" );
+//        }
+
+
+
+//
+//        System.out.println("Give points [0-100]:");
+//
+//        int grade= in.nextInt();
+//        if(grade<0){
+//            System.out.println("Grade:impossible!");
+//        }
+//        else  if(grade>=0 && grade<=49){
+//            System.out.println("Grade:Failed");
+//        }
+//        else if(grade>=50 && grade<=59)
+//        {
+//            System.out.println("Grade:1");
+//        }
+//        else if(grade>=60 && grade<=69)
+//        {
+//            System.out.println("Grade:2");
+//        }
+//        else if(grade>=70 && grade<=79)
+//        {
+//            System.out.println("Grade:3");
+//        }
+//        else if(grade>=80 && grade<=89)
+//        {
+//            System.out.println("Grade:4");
+//        }
+//        else if(grade>=90 && grade<=100)
+//        {
+//            System.out.println("Grade:5");
+//        }
+//        else {
+//            System.out.println("incredible!");
+//        }
+
+
+//
+//        System.out.println("Give the number:");
+//        int OEnum=in.nextInt();
+//        if(OEnum%2==0) {
+//            System.out.println("Number " + OEnum + " is even");
+//        }
+//        else {
+//            System.out.println("Number " + OEnum + " is odd");
+//        }
+
+//        System.out.println("Password?");
+//        String pass=in.nextLine();
+//        if(pass.equals("Caput Draconis"))
+//        {
+//            System.out.println("Welcome");
+//
+//        }
+//        else {
+//            System.out.println("Off with you!");
+//        }
+
+
+//        System.out.println("Enter the first string:");
+//        String s1=in.nextLine();
+//
+//        System.out.println("Enter the second string:");
+//        String s2=in.nextLine();
+//        if(s1.equals(s2)){
+//            System.out.println("Same");
+//        }
+//        else{
+//            System.out.println("Different");
+//        }
+
+//        System.out.print("How old are you?");
+//        int age=in.nextInt();
+//        if(age>=0 && age<=120){
+//            System.out.println("Ok");
+//        }
+//        else{
+//            System.out.println("impossible");
+//        }
+
+//        System.out.print("Give a year:");
+//        int year=in.nextInt();
+//        if(year%4==0 && year%100==0){
+//            if (year%400==0)
+//            {
+//
+//                System.out.println("The year is a leap year");
+//            }
+//            else {
+//                System.out.println("The year is not a leap year");
+//            }
+//        }
+//        else{
+//            System.out.println("The year is  not a leap year.");
+//        }
+
+
+        System.out.println("Value of the gift?");
+    double tax;
+        int gift=in.nextInt();
+if(gift<5000){
+    System.out.println("No tax!");
+}
+else if(gift>=5000 && gift<25000)
+{
+    tax=((gift-5000)*0.08)+100;
+}
+else if(gift>=25000 && gift<55000){
+
+    tax=((gift-25000)*0.1)+1700;
+}
+
+else if(gift>=55000 && gift<200000){
+
+    tax=((gift-55000)*0.12)+4700;
+}
+
+else if(gift>=200000 && gift<1000000){
+
+    tax=((gift-200000)*0.15)+22100;
+}
+else{    tax=((gift-1000000)*0.17)+142100;
+}
+        System.out.println("Tax: ");
+    }
+}
